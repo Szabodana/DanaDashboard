a1b366906027030c61847891386bcb19e74cae3b
Dani <szabo.f.daniel@gmail.com>
2024-01-15 02:19:53 +0100

    small bug fix with declarations

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -3,20 +3,19 @@ import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
-import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
 
 export default async function Page({
-                                       searchParams,
-                                   }: {
+    searchParams,
+    }: {
     searchParams?: {
         query?: string;
         page?: string;
-    };
-}) {
+        };
+    }) {
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
-
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
