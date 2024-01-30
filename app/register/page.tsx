b596bddeda4fcf31a9097b339500715a5d85bb72
Dani <szabo.f.daniel@gmail.com>
2024-01-30 22:58:08 +0100

    modified the register page, added credentials

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,22 +4,31 @@ import Link from "next/link";
 export const metadata: Metadata = {
     title: 'Customers',
 };
-export default function Page() {
-    return <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className="mb-3 text-2xl font-serif">Registration Not Implemented</h1>
-        <p className="mb-4">
-            The registration functionality is not implemented yet.
-        </p>
-        <p className="mb-4">
-            For testing purposes, you can use the following account information:
-        </p>
-        <p>Email: asd@asd.com</p>
-        <p>Password: 123456</p>
 
-        <Link href="/">
-            <p className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Back to the login page Login
-            </p>
-        </Link>
-    </div>;
-}
+export default function Page() {
+    return (
+        <main className="flex items-center justify-center h-screen">
+            <div className="w-full max-w-[400px] p-8 bg-gray-50 rounded-lg shadow-md">
+                <form className="space-y-4">
+                    <h1 className="text-3xl font-serif mb-4">Registration Not Implemented</h1>
+                    <p className="text-gray-600 mb-4">
+                        The registration functionality is not implemented yet, for testing purposes use these credentials:
+                    </p>
+                    <div className="mb-4">
+                        <p className="text-gray-800 font-medium">Email:</p>
+                        <p className="text-gray-600">user@nextmail.com</p>
+                    </div>
+                    <div className="mb-4">
+                        <p className="text-gray-800 font-medium">Password:</p>
+                        <p className="text-gray-600">123456</p>
+                    </div>
+                    <Link href="/">
+                        <p className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 text-center">
+                            Go Back
+                        </p>
+                    </Link>
+                </form>
+            </div>
+        </main>
+    );
+};
